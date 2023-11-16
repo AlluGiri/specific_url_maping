5f31337491ff69afe7ef4e82028ad7a5590476c8
Allukuru Giri <allukurugiri@gmail.com>
2023-11-16 10:19:27 +0530

    first commit

---
+++ gt/urls.py
@@ -0,0 +1,8 @@
+from gt.views import *
+from django.urls import path
+
+app_name='Hard'
+urlpatterns=[
+    path('pandya/',pandya,name='pandya'),
+    
+]
---
+++ gt/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def pandya(request):
+    return render(request,'pandya.html')
